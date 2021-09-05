3d4c1be861dda1e47c52578fa7d2300908f7aaed
Israel Adefidipe <israeladefidipe@gmail.com>
2021-09-05 22:42:42 +0100

    created task componenent and used useState hook

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@
 
 
 import React from "react";
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appoinment',
+        day: 'feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at Church',
+        day: 'feb 6th at 12:30pm',
+        reminder: false,
+    },
+    {
+        id: 3,
+        text: 'Grocery shoppint',
+        day: 'feb 5th at 2:30pm',
+        reminder: true,
+    }
+])
 
 
   return (
     <div className="container">
       <Header /> 
+      <Tasks tasks = {tasks}/>
     </div>
   );
 }
